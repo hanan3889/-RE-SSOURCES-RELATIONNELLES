27575a961f63932220a43ee349d0b5ed7f205a15
hanan3889 <rebaia_hanan@yahoo.fr>
2026-03-17 16:34:28 +0100

    fix: add missing using directive in Program.cs

--- backend/RessourceRelationnel.Api/Program.cs
+++ backend/RessourceRelationnel.Api/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using RessourceRelationnel.Api;
 using RessourceRelationnel.Api.Services;
 using RessourceRelationnel.Infrastructure.Data;
 
